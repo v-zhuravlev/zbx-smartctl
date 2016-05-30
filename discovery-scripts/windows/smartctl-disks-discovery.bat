c7ccd71962ffaaf5c0318347e7cfbc369389168e
v-zhuravlev <v-zhuravlev@users.noreply.github.com>
2016-05-30 16:27:31 +0300

    added duplicates check (by serial number) for windows batch

--- discovery-scripts/windows/smartctl-disks-discovery.bat
+++ discovery-scripts/windows/smartctl-disks-discovery.bat
@@ -1,26 +1,32 @@
 @echo off
 setlocal enabledelayedexpansion
-set /a counter=0
-set /a params=0
-set smartctl_path=C:\usr\zabbix\smartmontools\bin\smartctl.exe
 
-FOR /F "tokens=1" %%i IN ('!smartctl_path! --scan') DO (
-	set /a params+=1
-)
+set smartctl_path=C:\usr\zabbix\smartmontools\bin\smartctl.exe
+set serials=dummy_serial
+set /a first=1
 
 echo {"data":[
 for /f "tokens=1 delims= " %%i in ('!smartctl_path! --scan') do (
-	set /a counter+=1
+	
+    set duplicate=0
+    for /F "tokens=3*" %%a in ('!smartctl_path! -i %%i ^| find "Serial Number"') do (
+        set serial=%%a
+    )
 
+        rem duplicate disk entity, check by serial number
+        for %%j in (!serials!) do (if %%j == !serial! (set /a duplicate+=1)) 
+        set serials=%serials% !serial!
+
+    
 	FOR /F "tokens=1" %%a IN ('!smartctl_path! -i %%i ^|find "SMART" ^| find /C "Available"') DO (
 		set /a smart_enabled=%%a
 	)
-
-	if not !counter! EQU !params! (
-		echo {"{#DISKNAME}":"%%i","{#SMART_ENABLED}":"!smart_enabled!"},
-	) else (
-		echo {"{#DISKNAME}":"%%i","{#SMART_ENABLED}":"!smart_enabled!"}
-	)
-
+    if not !duplicate! == 1 (
+            if not !first! ==1 (
+                echo ,
+            )
+            echo {"{#DISKNAME}":"%%i","{#SMART_ENABLED}":"!smart_enabled!"}
+            set /a first=0
+    )
 )
 echo ]}
