521cd819e841f11362ab7f99fc91bc144faa9c61
niki <niki91m@yandex.ru>
2019-09-12 18:53:56 +0500

    Bugfix: More than one disk Refactoring

--- discovery-scripts/windows/smartctl-disks-discovery.ps1
+++ discovery-scripts/windows/smartctl-disks-discovery.ps1
@@ -1,17 +1,21 @@
-# VERSION = 1.5
+# VERSION = 1.6
 $smartctl = "$Env:Programfiles\smartmontools\bin\smartctl.exe"
 
 if ((Get-Command $smartctl -ErrorAction SilentlyContinue) -eq $null)
-{ 
-   write-host "Unable to find smartctl. Check that smartmontools package is installed"
-   exit
+{
+    write-host "Unable to find smartctl. Check that smartmontools package is installed"
+    exit
 }
-
 
 $idx = 0
 $global_serials
 $smart_scanresults = & $smartctl "--scan-open"
-$smart_scanresults += & $smartctl "--scan-open" "-dnvme"
+$smart_nvme = & $smartctl "--scan-open" "-dnvme"
+# bug fix. more than one disk
+if ($smart_nvme)
+{
+    $smart_scanresults += $smart_nvme
+}
 
 $json = ""
 
@@ -19,7 +23,6 @@ write-host "{"
 write-host " `"data`":["
 foreach ($smart_scanresult in $smart_scanresults)
 {
-    
     $idx++;
     $disk_args = ""
     $disk_name = ""
@@ -27,26 +30,28 @@ foreach ($smart_scanresult in $smart_scanresults)
     $disk_model = ""
     $disk_sn = ""
 
-    if ($smart_scanresult -match '(-d) ([A-Za-z0-9,\+]+)'){
+    if ($smart_scanresult -match '(-d) ([A-Za-z0-9,\+]+)')
+    {
         $disk_args = $matches[1]+$matches[2]
     }
-    
+
     $disk_name = $smart_scanresult.Substring(0,$smart_scanresult.IndexOf(" "))
     $line = & $smartctl "-i" $disk_name $disk_args
-    
+
     #smart enabled?
     $smart_enabled = $line | select-string "SMART.+Enabled$"
-    if($smart_enabled) {
+    if($smart_enabled)
+    {
         $smart_enabled = 1
     } else {
         $smart_enabled = 0
     }
 
-    if($disk_args -like "*nvme*" -or $disk_name -like "*nvme*" ) {
+    if($disk_args -like "*nvme*" -or $disk_name -like "*nvme*" )
+    {
         $disk_type = "1"
         $smart_enabled = 1
     }
-
 
     # Device sn
     $sn = $line | select-string "serial number:"
@@ -81,67 +86,73 @@ foreach ($smart_scanresult in $smart_scanresults)
     {
         $disk_model+=" "+$model.trim()
     }
-    
-    
+
     # Is it HDD, SSD/NVMe or ODD
     # The SMART Values for HDD/SSD are different sometimes
     # I have 2 Discovery-Rules with Filtering 0 or 1.
     # 0 is for HDD
     # 1 is for SSD/NVMe
     # 2 is for ODD and will be ignored
-    $Drive = $line  | select-string "Rotation Rate:" 
+    $Drive = $line | select-string "Rotation Rate:"
     if($Drive -like "*Solid State Device*") {$disk_type = "1"}
     elseif ($Drive -like "*rpm*") {$disk_type = "0"}
-    
-    if($disk_type -eq "2") { 
+
+    if($disk_type -eq "2")
+    {
         #Can't determine, lets go extended
         $extended_line = & $smartctl "-a" $disk_name $disk_args
-        
+
         #search for Spin_Up_Time or Spin_Retry_Count
-        if ($extended_line  | select-string "Spin_" -CaseSensitive){
+        if ($extended_line | select-string "Spin_" -CaseSensitive)
+        {
             $disk_type = "0"
         }
         #search for SSD in uppercase
-        elseif ($extended_line  | select-string " SSD " -CaseSensitive){
+        elseif ($extended_line | select-string " SSD " -CaseSensitive)
+        {
             $disk_type = "1"
         }
         #search for NVMe
-        elseif ($extended_line  | select-string "NVMe" -CaseSensitive){
+        elseif ($extended_line | select-string "NVMe" -CaseSensitive)
+        {
             $disk_type = "1"
             $smart_enabled = "1"
         }
-        elseif ($extended_line | select-string "177 Wear_Leveling" -CaseSensitive) {
+        elseif ($extended_line | select-string "177 Wear_Leveling" -CaseSensitive)
+        {
             $disk_type = "1"
         }
-        elseif ($extended_line | select-string "231 SSD_Life_Left" -CaseSensitive) {
+        elseif ($extended_line | select-string "231 SSD_Life_Left" -CaseSensitive)
+        {
             $disk_type = "1"
         }
-        elseif ($extended_line | select-string "233 Media_Wearout_" -CaseSensitive) {
+        elseif ($extended_line | select-string "233 Media_Wearout_" -CaseSensitive)
+        {
             $disk_type = "1"
         }
     }
 
-    if ($sn) {
+    if ($sn)
+    {
         $disk_sn=$sn.trim()
-    
-        if ($global_serials -contains $disk_sn ){
+
+        if ($global_serials -contains $disk_sn )
+        {
             continue
             #skip duplicated disk, go to next one
-        } else {
+        } else
+        {
             #add only smart capable disks to global serials
-            if ($smart_enabled -eq 1){
+            if ($smart_enabled -eq 1)
+            {
                 $global_serials+=$disk_sn
             }
         }
     }
 
-
-    if ($idx -eq 1)
+    if ($idx -ne 1)
     {
-
-    } else
-    {
-        $json +=  ",`n"
+        $json += ",`n"
     }
     
     $json += "`t {`n " +
